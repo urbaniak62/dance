83d8eeb8a514fb497f99dd2eef7ad0f5be406bd9
chniak <urbaniak.n78@gmail.com>
2018-09-24 20:46:17 +0200

    videom welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,25 @@
 @section('content')
         <div class="container-fluid">
             {{-- <img id="bg-welcome"src="{{ asset('img.bg-welcome.jpg') }}" alt=""> --}}
-            <video autoplay="" class="l_o_h"  loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg" style="width:100%;top:0%;left:0%;margin:0px;">
+            <video autoplay="" class="l_o_h"  loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg" style="width:100%;margin-top:-10%;background-size:cover;margin:-10% 0 0 0 ;">
                 <source src="{{asset('video/video.webm')}}" type="video/webm">
                 {{-- <source src="https://ak3.picdn.net/shutterstock/videos/25457273/preview/stock-footage-emotional-and-graceful-contemporary-dance-performed-by-champions-slow-motion.mp4" type="video/mp4"></video> --}}
 
             @if (Route::has('login'))
-                <div class="top-right links">
+                {{-- <div class="top-right links"> --}}
                     @auth
                         <a href="{{ url('/home') }}">accueil</a>
                     @endauth
-                </div>
+                {{-- </div> --}}
             @endif
 
-            <div class="content">
+            {{-- <div class="content">
                 <div class="title m-b-md">
-                    <img src="{{asset('img/titre.png')}}" alt="">
+                    <h1 id="titre">COURS DE DANCE</h1>
                 </div>
-            </div>
+            </div> --}}
         </div>
         <div class="container">
-            <h1>COURS DE DANCE</h1>
+            <h1 id="titre">DECLIC DANCE</h1>
         </div>
 @endsection
