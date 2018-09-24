732a970deb3b15e53c9f6cd192ca0806f0aaea83
chniak <urbaniak.n78@gmail.com>
2018-09-24 20:16:02 +0200

    videom welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,11 @@
 
 @section('content')
         <div class="container-fluid">
-            <img id="bg-welcome"src="{{ asset('img.bg-welcome.jpg') }}" alt="">
+            {{-- <img id="bg-welcome"src="{{ asset('img.bg-welcome.jpg') }}" alt=""> --}}
+            <video autoplay="" class="l_o_h"  loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg" style="width:100%;top:0%;left:0%;margin:0px;">
+                <source src="{{asset('video/video.webm')}}" type="video/webm">
+                {{-- <source src="https://ak3.picdn.net/shutterstock/videos/25457273/preview/stock-footage-emotional-and-graceful-contemporary-dance-performed-by-champions-slow-motion.mp4" type="video/mp4"></video> --}}
+
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
