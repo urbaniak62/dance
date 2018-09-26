38503bcc649cc28c4f5db445eeba151b717d0803
chniak <urbaniak.n78@gmail.com>
2018-09-26 17:15:00 +0200

    programme dance thum card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lilita+One" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 
     <!-- Styles -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,74 +1,121 @@
-
 @extends('layouts.app')
 
 @section('content')
-        <div class="container-fluid">
-            {{-- <img id="bg-welcome"src="{{ asset('img.bg-welcome.jpg') }}" alt=""> --}}
-            {{-- <video autoplay="" class="l_o_h"  loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg" style="width:100%;background-size:cover;margin:-7% 0 0 0 ;">
-                <source src="{{asset('video/video.webm')}}" type="video/webm"> --}}
-                {{-- <source src="https://ak3.picdn.net/shutterstock/videos/25457273/preview/stock-footage-emotional-and-graceful-contemporary-dance-performed-by-champions-slow-motion.mp4" type="video/mp4"></video> --}}
-            <video autoplay="" class="l_o_h"  loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg" style="width:100%;background-size:cover;margin:-7% 0 0 0 ;">
-                <source src="{{asset('video/dance1.mp4')}}" type="video/mp4">
-            </video>
-            @if (Route::has('login'))
-                {{-- <div class="top-right links"> --}}
-                    @auth
-                        <a href="{{ url('/home') }}">accueil</a>
-                    @endauth
-                {{-- </div> --}}
-            @endif
-            <div class="container">
-                <h1 id="titre">DECLIC DANCE</h1>
-            </div>
+<div class="container-fluid">
+    {{-- <img id="bg-welcome" src="{{ asset('img.bg-welcome.jpg') }}" alt=""> --}}
+    {{-- <video autoplay="" class="l_o_h" loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg"
+        style="width:100%;background-size:cover;margin:-7% 0 0 0 ;">
+        <source src="{{asset('video/video.webm')}}" type="video/webm"> --}}
+        {{--
+        <source src="https://ak3.picdn.net/shutterstock/videos/25457273/preview/stock-footage-emotional-and-graceful-contemporary-dance-performed-by-champions-slow-motion.mp4"
+            type="video/mp4"></video> --}}
+    <video autoplay="" class="l_o_h" loop="" playsinline="" poster="https://ak3.picdn.net/shutterstock/videos/25457273/thumb/1.jpg"
+        style="width:100%;background-size:cover;margin:-7% 0 0 0 ;">
+        <source src="{{asset('video/dance1.mp4')}}" type="video/mp4">
+    </video>
+    @if (Route::has('login'))
+    {{-- <div class="top-right links"> --}}
+        @auth
+        <a href="{{ url('/home') }}">accueil</a>
+        @endauth
+        {{-- </div> --}}
+    @endif
+    <div class="container">
+        <h1 id="titre">DECLIC DANCE</h1>
+    </div>
 
+</div>
+<div class="container">
+    <div class="content mot">
+        <h1>LE MOT DU PRESIDENT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae eligendi, quo eos
+            rerum iusto laudantium vel accusantium deserunt nemo repudiandae provident quibusdam
+            dolores obcaecati nostrum soluta animi quidem cum corrupti? Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit.
+            Maiores eaque sequi esse maxime repudiandae natus porro culpa totam magnam nemo velit inventore soluta, a
+            ullam expedita.
+            Ut voluptas esse sed.</p>
+    </div>
+    <h3>nos formation dance ?</h3>
+</div>
+<div class="container programme">
+    <div class="box">
+        <div class="thumb">
+            <img src="{{ asset('img/tango.jpg') }}" alt="">
         </div>
+        <div class="detail">
+            <div class="content">
+                <i class="fas fa-music"></i>
+                <h3>Rock</h3>
+                <a href="#">En savoir plus</a>
+            </div>
+        </div>
+    </div>
+    <div class="box">
+        <div class="thumb">
+            <img src="{{ asset('img/salsa.jpg') }}" alt="">
+        </div>
+        <div class="detail">
+            <div class="content">
+                <i class="fas fa-music"></i>
+                <h3>Rock</h3>
+                <a href="#">En savoir plus</a>
+            </div>
+        </div>
+    </div>
+    <div class="box">
+        <div class="thumb">
+            <img src="{{ asset('img/salsa1.jpg') }}" alt="">
+        </div>
+        <div class="detail">
+            <div class="content">
+                <i class="fas fa-music"></i>
+                <h3>Rock</h3>
+                <a href="#">En savoir plus</a>
+            </div>
+        </div>
+    </div>
+    <div class="box">
+        <div class="thumb">
+            <img src="{{ asset('img/Tango.jpg') }}" alt="">
+        </div>
+        <div class="detail">
+            <div class="content">
+                <i class="fas fa-music"></i>
+                <h3>Rock</h3>
+                <a href="#">En savoir plus</a>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="box">
+        <div class="thumb">
+            <img src="{{ asset('img/salsa2.jpeg') }}" alt="">
+        </div>
+        <div class="detail">
+            <div class="content">
+                <i class="fas fa-music"></i>
+                <h3>Rock</h3>
+            </div>
+        </div>
+    </div>
+    <div class="box">
+        <div class="thumb">
+            <img src="{{ asset('img/dance1.jpg') }}" alt="">
+        </div>
+        <div class="detail">
+            <div class="content">
+                <i class="fas fa-music"></i>
+                <h3>Rock</h3>
+            </div>
+        </div>
+    </div> --}}
+</div>
+<div class="container-fluid">
+    <footer>
         <div class="container">
-            <div class="content mot">
-                <h1>LE MOT DU PRESIDENT</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae eligendi, quo eos
-                rerum iusto laudantium vel accusantium deserunt nemo repudiandae provident quibusdam
-                dolores obcaecati nostrum soluta animi quidem cum corrupti? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Maiores eaque sequi esse maxime repudiandae natus porro culpa totam magnam nemo velit inventore soluta, a ullam expedita.
-                Ut voluptas esse sed.</p>
-            </div>
-            <h3>nos formation dance ?</h3>
 
-                <div class="row">
-                        <div class="col-md-3 col-lg-3">
-                            <p>rock 4 tps</p>
-                            <img src="{{ asset('img/tango.jpg') }}" alt=""> 
-                        </div>
-                        <div class="col-md-3 col-lg-43danceSpe">
-                            <p>rock 6tps</p> 
-                            <img src="{{ asset('img/tango.jpg') }}" alt=""> 
-                        </div>
-                        <div class="col-md-3 coll-lg-3 danceSpe">
-                            <p>salsa</p>
-                            <img src="{{ asset('img/tango.jpg') }}" alt="">      
-                        </div>
-                        <div class="col-md-3 col-lg-3 danceSpe">
-                            <p>rumba</p>
-                            <img src="{{ asset('img/tango.jpg') }}" alt="">         
-                        </div>
-                </div> 
-                <div class="row">
-                        <div class="col-md-3 col-lg-3">
-                            <p>chacha</p>
-                            <img src="{{ asset('img/tango.jpg') }}" alt=""> 
-                        </div>
-                        <div class="col-md-3 col-lg-43danceSpe">
-                            <p>pasodoble</p> 
-                            <img src="{{ asset('img/tango.jpg') }}" alt=""> 
-                        </div>
-                        <div class="col-md-3 coll-lg-3 danceSpe">
-                            <p>valse lente</p>
-                            <img src="{{ asset('img/tango.jpg') }}" alt="">      
-                        </div>
-                        <div class="col-md-3 col-lg-3 danceSpe">
-                            <p>valse 6tps</p>
-                            <img src="{{ asset('img/tango.jpg') }}" alt="">         
-                        </div>
-                </div> 
         </div>
+    </footer>
+</div>
 
 @endsection
